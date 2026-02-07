c7700c2a4600b591573bd3bc812f40f4ea6f5be2
ishangautam1610-boop <ishan.gautam1610@gmail.com>
2026-02-08 02:27:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=vGJTaP6anOU", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dpfm9aqqa/video/upload/v1770497792/Elvis_Presley_-_Can_t_Help_Falling_In_Love_Official_Audio_yv8obn.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
